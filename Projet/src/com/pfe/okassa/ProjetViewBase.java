e3cf8169b2a2d1cef26d3c0990a65dbe846f1caa
olympekassa <olympekassa@113174b1-3bd8-b405-3634-905a77f90cd0>
2012-01-18 07:57:14 +0000

    version de 7h

--- Projet/src/com/pfe/okassa/ProjetViewBase.java
+++ Projet/src/com/pfe/okassa/ProjetViewBase.java
@@ -1,6 +1,11 @@
 package com.pfe.okassa;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import org.opencv.core.Size;
@@ -9,11 +14,14 @@ import org.opencv.highgui.Highgui;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Toast;
 
 public abstract class ProjetViewBase extends SurfaceView implements SurfaceHolder.Callback, Runnable {
     private static final String TAG = "Sample::SurfaceView";
@@ -21,6 +29,10 @@ public abstract class ProjetViewBase extends SurfaceView implements SurfaceHolde
     private SurfaceHolder       mHolder;
     private VideoCapture        mCamera;
     public ProgressDialog progressDialog;
+    
+    String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+    String fileName = "myPicture.jpg";
+
     
     public ProjetViewBase(Context context) {
         super(context);
@@ -108,8 +120,32 @@ public abstract class ProjetViewBase extends SurfaceView implements SurfaceHolde
 
             if (bmp != null) {
                 Canvas canvas = mHolder.lockCanvas();
+                               
                 if (canvas != null) {
                     canvas.drawBitmap(bmp, (canvas.getWidth() - bmp.getWidth()) / 2, (canvas.getHeight() - bmp.getHeight()) / 2, null);
+                    if(Projet_Main.click == true)
+                    {
+                    	 
+                    	try {
+                        	// Not sure if the / is on the path or not
+                            File f = new File(baseDir + File.separator + fileName);
+
+                            // Create file if it does not exist
+                            boolean success = f.createNewFile();
+                            if (success) {
+                                // File did not exist and was created
+                            	FileOutputStream out = new FileOutputStream(f);
+                            	boolean t = bmp.compress(Bitmap.CompressFormat.JPEG, 100, out) ;
+                            		if(t=true)
+                            		{
+                            			// File compression was succesfull
+                            		}
+                            } else {
+                                // File already exists
+                            }
+                        } catch (IOException e) {
+                        }
+                    }
                     mHolder.unlockCanvasAndPost(canvas);
                 }
                 bmp.recycle();
